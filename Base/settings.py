14f137ccc2fed5814e1c13a7c7f55f31240e8dcd
rvamshi1711 <68069158+rvamshi1711@users.noreply.github.com>
2020-09-14 17:10:07 +0530

    Update settings.py

--- Base/settings.py
+++ Base/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'Base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Craiglist',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST':'craiglist-clonee.herokuapp.com',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
